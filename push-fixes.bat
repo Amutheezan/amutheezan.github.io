22e8406cb3ed98c99a9be979b7c6c7cfbc7eb326
Amutheezan Sivagnanam <private.amu@gmail.com>
2026-06-12 17:18:55 -0500

    UI: theme switcher, dark mode fixes, footer gap, flex layout

--- push-fixes.bat
+++ push-fixes.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0"
 git add _sass/_liquid-glass.scss _sass/_color-themes.scss _sass/_masthead.scss _sass/_dark-mode.scss
-git add _sass/_interest-tags.scss
+git add _sass/_interest-tags.scss _sass/_sidebar.scss
 git add _data/research_interests.yml _data/navigation.yml
 git add _pages/research.html _pages/about.md _includes/item-research-tags.html
 git add _layouts/single.html _layouts/talk.html
 git add _publications/ _talks/ _workshops/ _projects/
-git commit -m "Fix interest tag colors, filter pills, correct research interest assignments, remove Research from nav"
+git commit -m "Fix sidebar dark mode box, interest tags, research hub improvements"
 git push
 echo.
 echo Done! Press any key to close.
