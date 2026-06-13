425226ed351607d8503a21ade76d08f6c997f48e
Amutheezan Sivagnanam <private.amu@gmail.com>
2026-06-13 11:47:39 -0500

    Auto-deploy: 2026-06-13 11:47:39

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
       }
     },
     closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true /* allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source. */
   });
 
 });
